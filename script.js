862cacab1100d98e305a1e37afdad732a664b1ee
AStrayker <astrajker@gmail.com>
2024-08-01 16:04:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.style.display === 'block') {
+        sidebar.style.display = 'none';
+    } else {
+        sidebar.style.display = 'block';
+    }
+}
+
+// Здесь будет функциональность для добавления, редактирования, удаления объявлений
+// и создания, управления учетными записями без подтверждения
