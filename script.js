e0d28722f43ca9ceafc312ec71993310ebc8ebc9
AStrayker <astrajker@gmail.com>
2024-08-02 10:40:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-document.getElementById('toggleTheme').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-});
-
-const addFieldButton = document.getElementById('addFieldButton');
-const additionalFields = document.getElementById('additionalFields');
-let fieldCount = 0;
-
-addFieldButton.addEventListener('click', () => {
-    if (fieldCount < 15) {
-        const fieldDiv = document.createElement('div');
-        const fieldName = document.createElement('input');
-        fieldName.setAttribute('type', 'text');
-        fieldName.setAttribute('placeholder', 'Назва поля');
-
-        const fieldValue = document.createElement('input');
-        fieldValue.setAttribute('type', 'text');
-        fieldValue.setAttribute('placeholder', 'Значення');
-
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'X';
-        removeButton.addEventListener('click', () => {
-            additionalFields.removeChild(fieldDiv);
-            fieldCount--;
-        });
-
-        fieldDiv.appendChild(fieldName);
-        fieldDiv.appendChild(fieldValue);
-        fieldDiv.appendChild(removeButton);
-        additionalFields.appendChild(fieldDiv);
-
-        fieldCount++;
+﻿function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.style.display === 'block') {
+        sidebar.style.display = 'none';
+    } else {
+        sidebar.style.display = 'block';
     }
-});
+}
+
+// Здесь будет функциональность для добавления, редактирования, удаления объявлений
+// и создания, управления учетными записями без подтверждения
